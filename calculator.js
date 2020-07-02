d00827ad0d3a58a90f8b2a1c2a773b8090cf2284
Emmanuel Osei Mensah <mancuniamoe@gmail.com>
2020-07-02 16:41:54 +0000

    new calculator

--- calculator.js
+++ calculator.js
@@ -3,6 +3,11 @@ var c_tax;
 var tax;
 var n_amnt;
 
+//..............................
+var nhil_getfund;
+var vat;
+var result;
+
 
 //
 function PAYE(amnt){
@@ -44,31 +49,64 @@ else{
 
 
 // alert(c_tax);
-document.getElementById("paye_out").innerHTML=c_tax.toFixed(2); 
+document.getElementById("paye_out").value=c_tax.toFixed(2); 
 }
 
- function VAT(amnt){
-     var my_vat;
-     amnt=parseFloat(amnt);
-    //  var flat= document.getElementById("flat").value;
-     var nhil = amnt * .025;
-     nhil+=nhil;
-     console.log(nhil);
-     //no flat
-          my_vat = (amnt+nhil);
-          console.log(my_vat);
-          if (document.getElementById('cst').checked) {
-            var cst= amnt*.09;
-            my_vat+=cst;
-        }
-          my_vat *=0.125;
-          console.log(my_vat);
-     document.getElementById("vat_out").innerHTML=my_vat.toFixed(2); 
+//  function VAT(amnt){
+//      var my_vat;
+//      amnt=parseFloat(amnt);
+//     //  var flat= document.getElementById("flat").value;
+//      var nhil = amnt * .025;
+//      nhil+=nhil;
+//      console.log(nhil);
+//      //no flat
+//           my_vat = (amnt+nhil);
+//           console.log(my_vat);
+//           if (document.getElementById('cst').checked) {
+//             var cst= amnt*.09;
+//             my_vat+=cst;
+//         }
+//           my_vat *=0.125;
+//           console.log(my_vat);
+//      document.getElementById("vat_out").innerHTML=my_vat.toFixed(2); 
      
+//  }
 
+//calculate CST
+function cst(amnt){
 
- }
+    amnt=parseFloat(amnt);
+    result=amnt*.09;
+    result=result.toFixed(2)
 
+    document.getElementById("cst_out").value=result;
+
+return result;
+
+}
+
+//calculate Flat
+function flat(amnt){
+
+    amnt=parseFloat(amnt);
+    result=amnt*.03;
+    result=result.toFixed(2)
+
+    document.getElementById("vat_p_flat").value=result;
+return result;
+
+}
+
+function v_sales(amnt){
+
+    amnt=parseFloat(amnt);
+
+ nhil= amnt*.025;
+ nhil*=2;
+ amnt+=nhil;
+ result=amnt*.125;
+
+}
 
 
 
@@ -76,9 +114,10 @@ document.getElementById("paye_out").innerHTML=c_tax.toFixed(2);
 
 
 function calc_paye(){
-    var in_amnt= document.getElementById("in_amnt").value;
+    var in_amnt= document.getElementById("paye_in").value;
     PAYE(in_amnt);
     }
+
 
     function calc_vat(){
         var in_amnt= document.getElementById("in_amnt").value;
